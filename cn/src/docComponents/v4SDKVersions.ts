1dafa85e11cddece79bf6338d1352ad6e6d89c27
陈轲 <chenke@xd.com>
2025-07-08 18:03:23 +0800

    docs:  排行榜文档 #no-ticket

--- cn/src/docComponents/v4SDKVersions.ts
+++ cn/src/docComponents/v4SDKVersions.ts
@@ -1,6 +1,6 @@
-const taptapUnity = "4.6.3"
+const taptapUnity = "4.7.0"
 const taptapIos = "4.6.3"
-const taptapAndroid = "4.6.3"
+const taptapAndroid = "4.7.0"
 const taptapUnreal = "3.29.2"
 
 const v4SDKVersions = {
